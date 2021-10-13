63b11e8376e2c0f7fd380bfb3f998362b317ac71
Pontus <pontus@minabastapolare.se>
2021-10-13 23:54:30 +0200

    Some imporovements, fadein

--- src/index.js
+++ src/index.js
@@ -5,16 +5,17 @@ import Test from './inc/test';
 (function(w) {
     
     
-    w.addEventListener('load', function() {
+    w.addEventListener('DOMContentLoaded', function() {
         var asd = 'test';
         console.log('ready');
         let t = new Test({ test: 1, asd:'asd'});
         t.Run();
 
-        w.document.body.style = '';
-        w.document.body.classList.add('opacity-100');
-        w.document.body.classList.remove('opacity-0');
-        w.document.body.classList.remove('hidden');
+        // w.document.body.style = '';
+        // w.document.body.classList.add('opacity-100');
+        w.document.body.classList.add('loaded');
+        // w.document.body.classList.remove('opacity-0');
+        // w.document.body.classList.remove('hidden');
     });
 
 })(window);
